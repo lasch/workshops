e210c8a4a02a20b6f241e1b9ec88bf543bf6dea1
Linsong Chu <lchu@us.ibm.com>
2023-11-20 21:55:48 -0500

    add final ckpt conversion script

---
+++ pretraining/fms_ckpt_to_hf.py
@@ -0,0 +1,35 @@
+import torch
+from fms.models.hf import to_hf_api
+from fms.models.llama import LLaMAConfig, LLaMA
+from torch.distributed._shard.checkpoint import (
+    FileSystemReader,
+    load_state_dict,
+)
+
+load_path = "/lustre/t5/workshops/pretraining/ckpt/checkpoints/step_100000_ckp"
+save_path = "/lustre/t5/workshops/pretraining/ckpt/hf/step_100000_ckp.pth"
+
+llama_config = LLaMAConfig(
+    src_vocab_size=32000,
+    emb_dim=4096,
+    norm_eps=1e-05,
+    nheads=32,
+    nlayers=32,
+    hidden_grow_factor=11008/4096,
+    multiple_of=1,
+    activation_fn="silu",
+    max_expected_seq_len=2048,
+)
+
+with torch.device("meta"):
+    model = LLaMA(llama_config, orig_init=True)
+
+state_dict = {"model_state": model.state_dict()}
+load_state_dict(
+    state_dict=state_dict, storage_reader=FileSystemReader(load_path), no_dist=True
+)
+model.load_state_dict(state_dict["model_state"])
+
+hf_model = to_hf_api(model)
+
+torch.save(hf_model.state_dict(), save_path)
